8eaf4ca86ce5157d215d4c60c35761e1a90b7444
Moher <47338928+newmon13@users.noreply.github.com>
2025-04-17 16:00:36 +0200

    Refactored wifi_setup.py

--- mp_resource/config.py
+++
@@ -1,9 +0,0 @@
-WIFI_CONFIG = {
-    'ssid': 'ssid_example',
-    'password': 'password_example'
-}
-
-SERVER_CONFIG = {
-    'port': 8081,
-    'host': '0.0.0.0'
-}
--- mp_resource/wifi_setup.py
+++ mp_resource/wifi_setup.py
@@ -8,21 +8,24 @@ WIFI_PASSWORD = config.WIFI_CONFIG.get("password")
 def connect_wifi():
     wlan = network.WLAN(network.STA_IF)
     wlan.active(True)
-    if not wlan.isconnected():
-        print('Connecting to network...')
-        wlan.connect(WIFI_SSID, WIFI_PASSWORD)
-        max_wait = 10
-        while max_wait > 0:
-            if wlan.isconnected():
-                break
-            max_wait -= 1
-            print('Waiting for connection...')
-            time.sleep(1)
+    if establish_wifi_connection(wlan):
+        print_connection_details(wlan)
+    return wlan.isconnected()
 
+def establish_wifi_connection(wlan: network.WLAN):
+    print('Connecting to network...')
+    wlan.connect(WIFI_SSID, WIFI_PASSWORD)
+    max_wait = 10
+    while max_wait > 0:
+        if wlan.isconnected():
+            break
+        max_wait -= 1
+        print('Waiting for connection...')
+        time.sleep(1)
+
+    return wlan.isconnected()
+
+def print_connection_details(wlan: network.WLAN):
     if wlan.isconnected():
         print('Connected to WiFi')
         print('Network config:', wlan.ifconfig())
-        return True
-    else:
-        print('Could not connect to WiFi')
-        return False
