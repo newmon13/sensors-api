c5154da511bfcdcee96850bd0cbe8e4d680c8631
Moher <47338928+newmon13@users.noreply.github.com>
2025-04-17 19:04:25 +0200

    Renamed methods

--- mp_resource/web_server.py
+++ mp_resource/web_server.py
@@ -6,7 +6,7 @@ import smoke_sensor
 app = Microdot()
 
 @app.route('/api/water-sensor')
-def get_data(request):
+def get_water_sensor_data(request):
     value = water_sensor.measure_water_level()
 
     data = {
@@ -16,6 +16,6 @@ def get_data(request):
     return Response(json.dumps(data), headers={'Content-Type': 'application/json'})
 
 @app.route('/api/smoke-sensor')
-def get_data(request):
+def get_smoke_sensor_data(request):
     value = smoke_sensor.measure_smoke_level()
     return Response(json.dumps(value), headers={'Content-Type': 'application/json'})
