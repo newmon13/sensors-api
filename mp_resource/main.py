25e2c992fc76ee9007ddad31d4c2dcc78ae45055
Moher <47338928+newmon13@users.noreply.github.com>
2025-04-17 14:26:17 +0200

    Added endpoint for retrieving data from water sensor

--- mp_resource/main.py
+++ mp_resource/main.py
@@ -1,12 +1,25 @@
+import network
+import time
+from lib.microdot import Microdot, Response
+import json
+import water_sensor
+import web_server
 import wifi_setup
 
-SSID = '_'
-PASSWORD = '123zxcvbnm'
+SSID = 'UPC1117673'
+PASSWORD = 'zvzukx7sswpmuweV'
 
 
 def main():
+
     if wifi_setup.connect_wifi(SSID, PASSWORD):
+
         print('Starting Microdot server...')
+        try:
+            web_server.app.run(port=8081, debug=True)
+        except KeyboardInterrupt:
+            print('Server stopped')
+
 
 if __name__ == '__main__':
     main()
