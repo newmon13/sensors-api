cc8857d1c1c75e6de1e50bc2b7e3e4c7a48b9d29
Moher <47338928+newmon13@users.noreply.github.com>
2025-04-29 14:47:15 +0200

    Updated config

---
+++ mp_resource/config/settings.py
@@ -0,0 +1,13 @@
+WIFI_CONFIG = {
+    'ssid': 'example_ssid',
+    'password': 'example_password'
+}
+
+BROKER_CONFIG = {
+    'host': '0.0.0.0',
+    'port': 1883    #default mosquitto port
+}
+
+SUPPORTED_SENSORS = ['WATER', 'SMOKE']
+
+INSTALLED_SENSORS = ['WATER', 'SMOKE']
